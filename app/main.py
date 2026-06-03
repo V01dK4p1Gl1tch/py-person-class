8077740359e1f69b3e3c3368964f7d5f69fffa06
V01dK4p1Gl1tch <mishatsareyv@gmail.com>
2026-06-03 22:27:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,18 +9,18 @@ class Person:
 
 def create_person_list(people_dicts: list) -> list:
     Person.people.clear()
-    
+
     [Person(p_dict["name"], p_dict["age"]) for p_dict in people_dicts]
-    
+
     result_list = []
     for p_dict in people_dicts:
         instance = Person.people[p_dict["name"]]
-        
+
         if p_dict.get("wife") is not None:
             setattr(instance, "wife", Person.people[p_dict["wife"]])
         elif p_dict.get("husband") is not None:
             setattr(instance, "husband", Person.people[p_dict["husband"]])
-            
+
         result_list.append(instance)
-        
+
     return result_list
